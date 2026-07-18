a135fd9d6e0839a49e32edb1a14b083e303c27ea
Yang HENG <heng.yang.ds@hotmail.com>
2026-07-18 19:46:45 +0800

    refactor: group CEL policy contracts

--- tests/unit/kernel/test_cel_topology_predicates.py
+++ tests/unit/kernel/test_cel_topology_predicates.py
@@ -10,7 +10,7 @@ import ethos_core.contracts.artifacts.topology as topology_contract
 from ethos_core.contracts.artifacts.topology import GeneratedArtifactTopologyDeclaration
 from ethos_core.contracts.artifacts.topology import load_generated_artifact_topology_declaration
 from ethos_core.contracts.artifacts.topology import path_policy_from_declaration
-from ethos_core.contracts.cel import evaluate_cel_predicate
+from ethos_core.contracts.policy.cel import evaluate_cel_predicate
 
 _PREFIX_RULE = 'facts.path == rule.prefix || facts.path.startsWith(rule.prefix + "/")'
 

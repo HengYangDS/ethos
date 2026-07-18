b670a297390411bee3cffd8fabcff374abbe0f17
Yang HENG <heng.yang.ds@hotmail.com>
2026-07-18 12:52:25 +0800

    test: consolidate lifecycle fixtures

--- tests/unit/cli/land/test_publication.py
+++ tests/unit/cli/land/test_publication.py
@@ -10,8 +10,8 @@ from tests.support.contract_helpers import adopt_and_commit
 from tests.support.contract_helpers import git
 from tests.support.contract_helpers import init_git_repo
 from tests.support.contract_helpers import seed_executed_proof
+from tests.support.contract_helpers import write_role_policy
 from tests.support.ethos_cli_runner import run_ethos
-from tests.support.ethos_cli_runner import write_role_policy
 
 
 def test_publish_reports_invalid_local_ci_fallback_evidence_manifest(
